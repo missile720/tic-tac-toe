bf404eea1a156096c46859f8017469a40eb2dfe9
Misael Bolainez <misaelbolainez@gmail.com>
2022-08-01 11:02:36 -0700

    feat(Winner): winner is anounced via model when three in a row is achieved by either user or computer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-//selects all spaces
-let spaces = document.querySelectorAll(".space");
+let board = [null,null,null,null,null,null,null,null,null];
 
-//loops through all spaces and adds click event listeners
-for(let i = 0; i < spaces.length; i++){
-    spaces[i].addEventListener("click", userClick);
+initialize();
+
+function initialize(){
+    //selects all spaces
+    let spaces = document.querySelectorAll(".space");
+
+    //loops through all spaces and adds click event listeners
+    for(let i = 0; i < spaces.length; i++){
+        spaces[i].addEventListener("click", userClick);
+    }
 }
+
+//creates connection from js to html modal
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {backdrop: "static"})
 
 //functions runs on user click
 function userClick(event){
@@ -19,6 +28,16 @@ function userClick(event){
     //remove event listener
     event.target.removeEventListener("click", userClick);
 
+    //grabs elements id
+    let identity = event.target.id;
+    //grabs number
+    let number = parseInt(identity.charAt(identity.length-1));
+    //sets board to 1
+    board[number-1] = 1;
+
+    //check win condition
+    winCondition();
+
     //function runs for computer
     computerClick(width);
 }
@@ -30,13 +49,23 @@ function computerClick(width){
         //selects random number from 1-9
         let num = Math.ceil(Math.random() * 9);
         //stores whats inside element
-        let text = document.querySelector(`.box${num}`).innerHTML;
+        let text = document.querySelector(`#box${num}`).innerHTML;
         //checks if element is empty
         if(text === ""){
             //if empty fills in with O
-            document.querySelector(`.box${num}`).innerHTML = `<img src="./img/o.png" width="${width}px" height="${width}px">`;
+            document.querySelector(`#box${num}`).innerHTML = `<img src="./img/o.png" width="${width}px" height="${width}px">`;
             //remove event listener
-            document.querySelector(`.box${num}`).removeEventListener("click", userClick);
+            document.querySelector(`#box${num}`).removeEventListener("click", userClick);
+            
+
+            //grabs elements id
+            let identity = num;
+            //sets board to 0
+            board[identity-1] = 0;
+
+            //check win condition for computer
+            winConditionComputer();
+            
             //breaks loop
             check = false;
         }
@@ -60,5 +89,167 @@ function computerClick(width){
             check = false;
         }
     }
+}
+
+//function runs to see if user wins
+function winCondition(){
+    //init all wins conditions
+    let row0 = 0;
+    let row1 = 0;
+    let row2 = 0;
+    let col0 = 0;
+    let col1 = 0;
+    let col2 = 0;
+    let cross0 = 0;
+    let cross1 = 0;
     
+    //loops through current board
+    for(let i = 0; i < board.length; i++){
+        //checks for user spaces
+        if(board[i] === 1){
+            //for each user space adds to that win condition
+            switch(i){
+                case 0:
+                    row0++;
+                    col0++;
+                    cross0++;
+                    break;
+                case 1:
+                    row0++;
+                    col1++;
+                    break;
+                case 2:
+                    row0++;
+                    col2++;
+                    cross1++;
+                    break;
+                case 3:
+                    row1++;
+                    col0++;
+                    break;
+                case 4:
+                    row1++;
+                    col1++;
+                    cross0++;
+                    cross1++;
+                    break;
+                case 5:
+                    row1++;
+                    col2++;
+                    break;
+                case 6:
+                    row2++;
+                    col0++;
+                    cross1++;
+                    break;
+                case 7:
+                    row2++;
+                    col1++;
+                    break;
+                case 8:
+                    row2++;
+                    col2++;
+                    cross0++;
+                    break;
+            }
+        }
+    }
+
+    //checks all win conditions for three in a row
+    if(row0 === 3 || row1 === 3 || row2 === 3 || col0 === 3 || col1 === 3 || col2 === 3 || cross0 === 3 || cross1 === 3){
+        myModal.toggle();
+        document.getElementById("winner").innerHTML = "You win!!!! :D";
+    }
+}
+
+//functions runs to check to see if computer wins
+function winConditionComputer(){
+    //init all wins conditions
+    let row0 = 0;
+    let row1 = 0;
+    let row2 = 0;
+    let col0 = 0;
+    let col1 = 0;
+    let col2 = 0;
+    let cross0 = 0;
+    let cross1 = 0;
+    
+    for(let i = 0; i < board.length; i++){
+        //checks for computer spaces
+        if(board[i] === 0){
+            //for each computer space adds to that win condition
+            switch(i){
+                case 0:
+                    row0++;
+                    col0++;
+                    cross0++;
+                    break;
+                case 1:
+                    row0++;
+                    col1++;
+                    break;
+                case 2:
+                    row0++;
+                    col2++;
+                    cross1++;
+                    break;
+                case 3:
+                    row1++;
+                    col0++;
+                    break;
+                case 4:
+                    row1++;
+                    col1++;
+                    cross0++;
+                    cross1++;
+                    break;
+                case 5:
+                    row1++;
+                    col2++;
+                    break;
+                case 6:
+                    row2++;
+                    col0++;
+                    cross1++;
+                    break;
+                case 7:
+                    row2++;
+                    col1++;
+                    break;
+                case 8:
+                    row2++;
+                    col2++;
+                    cross0++;
+                    break;
+            }
+        }
+    }
+
+    //checks all win conditions for three in a row
+    if(row0 === 3 || row1 === 3 || row2 === 3 || col0 === 3 || col1 === 3 || col2 === 3 || cross0 === 3 || cross1 === 3){
+        myModal.toggle();
+        document.getElementById("winner").innerHTML = "You lost :( try again?";
+    }
+}
+
+//dom that executes when user clicks on the reset button
+document.getElementById("reset").onclick = function(){reset()};
+document.getElementById("reset1").onclick = function(){reset()};
+document.getElementById("reset2").onclick = function(){reset()};
+
+//resets the game
+function reset(){
+    board = [null,null,null,null,null,null,null,null,null];
+
+    //selects all spaces
+    let spaces = document.querySelectorAll(".space");
+
+    //loops through all spaces and adds click event listeners
+    for(let i = 0; i < spaces.length; i++){
+        spaces[i].innerHTML = "";
+        spaces[i].removeEventListener("click", userClick);
+    }
+
+    //runs initial set up code
+    initialize();
 }
